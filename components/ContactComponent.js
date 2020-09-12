87d5d6ff5011a8dc2e20cfad3682bc5e7e6ded1f
nil-sj <niladri123@gmail.com>
2020-09-11 22:44:20 -0400

    Week 1 - Workshop Task 2

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { View, Text, ScrollView } from 'react-native';
+import { Text, ScrollView } from 'react-native';
+import { Card } from 'react-native-elements';
 
 class Contact extends Component {
 
@@ -10,7 +11,22 @@ class Contact extends Component {
     render() {
         return (
             <ScrollView>
-                
+                <Card 
+                title='Contact Information'
+                wrapperStyle={{margin: 10}}>
+                    <Text
+                        style={{marginBottom: 10}}>
+                        1 Nucamp Way{"\n"}
+                        Seattle, WA 98001{"\n"}
+                        U.S.A.
+                    </Text>
+                    <Text>
+                        Phone: 1-206-555-1234
+                    </Text>
+                    <Text>
+                        Email: campsites@nucamp.co
+                    </Text>
+                </Card>
             </ScrollView>
         );
     }
