511b5f3215e5e7b83877b46e96b3539a0c326fff
nil-sj <niladri123@gmail.com>
2020-10-03 11:35:35 -0400

    Week 4 - Workshop Task 2

--- components/LoginComponent.js
+++ components/LoginComponent.js
@@ -170,6 +170,21 @@ class RegisterTab extends Component {
         this.setState({ imageUrl: processedImage.uri });
     }
 
+    getImageFromGallery = async () => {
+        const cameraRollPermission = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+
+        if (cameraRollPermission.status === 'granted') {
+            const capturedImage = await ImagePicker.launchImageLibraryAsync({
+              allowsEditing: true,
+              aspect: [1, 1]
+            });
+            if (!capturedImage.cancelled) {
+              this.processImage(capturedImage.uri)
+              console.log(capturedImage);
+            }
+        }
+    }
+
     handleRegister() {
         console.log(JSON.stringify(this.state));
         if (this.state.remember) {
@@ -195,6 +210,10 @@ class RegisterTab extends Component {
                         <Button
                             title='Camera'
                             onPress={this.getImageFromCamera}
+                        />
+                        <Button
+                            title='Gallery'
+                            onPress={this.getImageFromGallery}
                         />
                     </View>
                     <Input
