8723155350ce6f11e2d790a8f5f81d4fcc82d739
nil-sj <niladri123@gmail.com>
2020-10-03 12:21:15 -0400

    Week 4 - Workshop Task 3

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -317,22 +317,25 @@ const MainNavigator = createDrawerNavigator(
 
 class Main extends Component {
 
+    showNetInfo = async () => {
+        const connectionInfo = await NetInfo.fetch();
+        
+        (Platform.OS === 'ios') ?
+        Alert.alert('Initial Network Connectivity Type:', connectionInfo.type)
+        : ToastAndroid.show('Initial Network Connectivity Type: ' +
+        connectionInfo.type, ToastAndroid.LONG);
+    
+        this.unsubscribeNetInfo = NetInfo.addEventListener(connectionInfo => {
+            this.handleConnectivityChange(connectionInfo);
+        });
+    }
+
     componentDidMount() {
         this.props.fetchCampsites();
         this.props.fetchComments();
         this.props.fetchPromotions();
         this.props.fetchPartners();
-
-        NetInfo.fetch().then(connectionInfo => {
-            (Platform.OS === 'ios') ?
-                Alert.alert('Initial Network Connectivity Type:', connectionInfo.type)
-                : ToastAndroid.show('Initial Network Connectivity Type: ' +
-                    connectionInfo.type, ToastAndroid.LONG);
-        });
-
-        this.unsubscribeNetInfo = NetInfo.addEventListener(connectionInfo => {
-            this.handleConnectivityChange(connectionInfo);
-        });
+        this.showNetInfo();
     }
 
     componentWillUnmount() {
